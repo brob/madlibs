6ea979ae8f3f7803cbe77e6f3debb47737f96805
Bryan Robinson <bryan@sanity.io>
2021-07-01 11:28:25 -0400

    Fixes case sensitive issue

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -5,7 +5,7 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 import madlib from './madlib'
-import madlibField from './objects/madlibField'
+import madlibField from './objects/madLibField'
 import userLib from './userLib'
 
 // Then we give our schema to the builder and provide the result to Sanity
